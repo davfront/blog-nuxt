b558978b2033f3040d62c198a654cb4829389e46
davfront <contact@david-pereira.net>
2022-10-21 11:04:41 +0200

    static hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@ import svgLoader from 'vite-svg-loader'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+  ssr: false,
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
