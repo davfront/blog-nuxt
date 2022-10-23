b4a9dd7c4806c77b08261c239508ebfb7c1837c7
davfront <contact@david-pereira.net>
2022-10-23 20:39:32 +0200

    enable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import svgLoader from 'vite-svg-loader'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: 'static',
-  ssr: false,
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
