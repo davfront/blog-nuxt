d080018a1a0c2d1048934eec667ecd53ca471a85
davfront <contact@david-pereira.net>
2022-10-26 15:16:17 +0200

    layout footer

---
+++ components/LayoutFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="text-sm opacity-50 mt-10">
+    {{ $dayjs().format('YYYY') }} © David Pereira
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
     <LayoutHeader />
-    <main class="flex justify-center">
-      <div class="flex-auto min-w-0 max-w-2xl mx-6 my-12 md:mx-8 md:my-16">
+    <div class="mx-auto max-w-2xl px-6 py-12 md:px-8 md:py-16">
+      <main>
         <slot />
-      </div>
-    </main>
+      </main>
+      <LayoutFooter />
+    </div>
   </div>
 </template>
 
 <script setup>
 import LayoutHeader from "~/components/LayoutHeader.vue"
+import LayoutFooter from "~/components/LayoutFooter.vue"
 </script>
