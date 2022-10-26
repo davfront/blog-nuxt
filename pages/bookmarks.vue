e6440a5c0c5cc8372a750390ac4506a1d4ad119a
davfront <contact@david-pereira.net>
2022-10-26 15:38:15 +0200

    first bookmarks

--- pages/bookmarks.vue
+++ pages/bookmarks.vue
@@ -1,12 +1,52 @@
 <template>
   <Prose>
     <h1>Bookmarks</h1>
-    <p>Coming soon...</p>
+
+    <section v-for="(category, index) in bookmarkConfig" :key="index">
+      <h3>{{category.category}}</h3>
+      <ul>
+        <li v-for="bookmark in category.bookmarks" :key="index">
+          <a
+            :href="bookmark.url"
+            target="_blank"
+            :title="bookmark.title"
+          >
+            {{ bookmark.title }}
+          </a>
+          {{ bookmark.description ? `- ${bookmark.description}` : '' }}
+        </li>
+      </ul>
+    </section>
+
   </Prose>
 </template>
 
 <script setup lang="ts">
 import Prose from "~/components/Prose.vue"
+import { reactive } from "vue";
+
+const bookmarkConfig = reactive([
+  {
+    category: "UX/UI",
+    bookmarks: [
+      {
+        title: "Open UI",
+        url: "https://open-ui.org",
+        description: "Comparison of components from different design systems",
+      }
+    ]
+  },
+  {
+    category: "Resources",
+    bookmarks: [
+      {
+        title: "Iconify",
+        url: "https://iconify.design",
+        description: "All popular icon sets in one framework",
+      }
+    ]
+  }
+])
 
 useHead({
   title: 'Bookmarks',
